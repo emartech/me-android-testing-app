a5c5f543575c9005a30f5c58b478ed89e1922cad
Johan Persson <johan.persson@emarsys.com>
2019-08-29 14:32:03 +0200

    fix: give user some more feedback

--- app/src/main/java/com/example/mobile_engage/testing/MainActivity.kt
+++ app/src/main/java/com/example/mobile_engage/testing/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.mobile_engage.testing
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.emarsys.core.api.result.CompletionListener
 import com.emarsys.Emarsys
 
-class MainActivity : AppCompatActivity() {
-    
+class MainActivity : AppCompatActivity(), CompletionListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,10 +20,25 @@ class MainActivity : AppCompatActivity() {
         sendLatencyTestEvents()
     }
 
+    override fun onCompleted(errorCause: Throwable?) {
+        if(errorCause == null) setStatus(R.string.done_label)
+        else {
+            setStatus(R.string.error_label)
+            Toast.makeText(this, errorCause.message, Toast.LENGTH_LONG).show()
+        }
+    }
+
     private fun sendLatencyTestEvents() {
         val customEventName = "latency-test"
+
+        setStatus(R.string.in_progress_label)
         Emarsys.trackCustomEvent(customEventName, null)
         Emarsys.trackCustomEvent(customEventName, null)
-        Emarsys.trackCustomEvent(customEventName, null)
+        Emarsys.trackCustomEvent(customEventName, null, this)
+    }
+
+    private fun setStatus(resId: Int) {
+        val statusTextView: TextView = findViewById(R.id.statusTextView)
+        statusTextView.setText(resId)
     }
 }
